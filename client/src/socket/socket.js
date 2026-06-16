28d2d9811eea94cc5028b3e78595e4b504e0df01
Anand yadav <ay108679@gmail.com>
2026-06-16 13:21:04 +0530

    url update

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:5000";
+const URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
 
 export const createSocketConnection = (token) => {
     return io(URL, {
